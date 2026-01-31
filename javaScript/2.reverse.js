84f2d893194403cb983a436b8f2908918a4ce241
kamalcodezen <kamaluddin7908@gmail.com>
2026-02-01 00:24:51 +0530

    array reverse practice

---
+++ javaScript/2.reverse.js
@@ -0,0 +1,37 @@
+
+// Array reverse ---------
+
+let nums = [1, 2, 3, 4, 5, 6, 7, 8, 9];
+
+let change = nums.slice().reverse();
+console.log(change);
+
+console.log(nums);
+
+
+let rev = [];
+for (let num of nums) {
+    // console.log(num);
+    // rev = [num, ...rev];
+    rev = num + rev;
+}
+console.log(rev);
+let str = rev.split(""); //Array string banalam
+console.log(str);
+
+
+let reVerse = []
+for (let i = 0; i < nums.length; i++) {
+    // console.log(nums[i]);
+    let letter = nums[i];
+    reVerse.unshift(letter);
+}
+console.log(reVerse);
+
+
+let revArray = [];  // different reverse way
+for (i = nums.length - 1; i >= 0; i--) {
+    // console.log(nums[i]);
+    let let_ter = nums[i];
+    revArray.push(let_ter);
+}
